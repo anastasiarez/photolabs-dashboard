3be02f8e46679eeeb29fd09a6946cf534d499230
Mona Lisa <anastasiia.rn@gmail.com>
2023-10-08 12:40:59 -0400

    commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,67 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = { loading: false, focused: null };
 
-    return <main className={dashboardClasses} />;
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused !== null
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused
+      ? data.filter(panel => this.state.focused === panel.id)
+      : data
+    ).map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onClick={() => this.handlePanelClick(panel.id)}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
+
+  handlePanelClick = (panelId) => {
+    // Toggle between focused and unfocused views
+    this.setState((prevState) => ({
+      focused: prevState.focused === panelId ? null : panelId
+    }));
+  };
 }
 
 export default Dashboard;
+
+
