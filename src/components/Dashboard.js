b7fab7201ff8bf41aee3e944be0f2848857cc789
Mona Lisa <anastasiia.rn@gmail.com>
2023-10-16 14:58:07 -0400

    commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,13 @@ import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+ } from "helpers/selectors";
+
 const data = [
   {
     id: 1,
@@ -27,33 +34,70 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false, focused: null };
+  state = {
+    loading: false,
+    focused: null,
+    photos: [],
+    topics: []
+  };
+
+  componentDidMount() {
+
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+
+
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos: photos,
+          topics: topics
+        });
+      });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
 
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused !== null
     });
-
+    
     if (this.state.loading) {
       return <Loading />;
     }
-
+    
+    console.log(this.state);
+    
     const panels = (this.state.focused
       ? data.filter(panel => this.state.focused === panel.id)
       : data
-    ).map(panel => (
-      <Panel
+      ).map(panel => (
+        <Panel
         key={panel.id}
         id={panel.id}
         label={panel.label}
         value={panel.value}
         onClick={() => this.handlePanelClick(panel.id)}
-      />
-    ));
-
-    return <main className={dashboardClasses}>{panels}</main>;
-  }
-
+        />
+        ));
+        
+        return <main className={dashboardClasses}>{panels}</main>;
+      }
+      
   handlePanelClick = (panelId) => {
     // Toggle between focused and unfocused views
     this.setState((prevState) => ({
